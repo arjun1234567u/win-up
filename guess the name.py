17cd3475e4fb9d1f155f3c3987e017af2f630880
arjun1234567u <85978334+arjun1234567u@users.noreply.github.com>
2021-06-15 20:45:09 -0700

    Add files via upload

---
+++ guess the name.py
@@ -0,0 +1,69 @@
+name = input("Enter your name")
+print ("your name is "+ name)
+lastname = input("Enter your lastname")
+print ("your lastname is "+ lastname)
+score = 0
+name = "Bob"
+guess = input ("guess the name that starts with a B and ends with a b ")
+if guess == name:
+    print ("good job")
+    score += 1
+else:
+    print ("wrong")
+name = "photo"
+guess = input ("guess the word that starts with a pho ")
+if guess == name:
+    print ("good job")
+    score += 1
+else:
+    print ("wrong")
+name = "hello"
+guess = input ("guess the word that starts with hell ")
+if guess == name:
+    print ("good job")
+    score += 1
+else:
+    print ("wrong")
+name ="supper"
+guess = input ("guess the word that starts with sup ")
+if guess == name:
+    print ("good job")
+    score += 1
+else:
+    print ("wrong")
+name = "car"
+guess = input ("guess the word that starts with a c and a r at the end ")
+if guess == name:
+    print ("good job")
+    score += 1
+else:
+    print ("wrong")
+name = "ninety"
+guess = input ("guess the word that starts with a number ")
+if guess == name:
+    print ("good job")
+    score += 1
+else:
+    print ("wrong")
+name = "varsha"
+guess = input ("this is a name that starts with a var ")
+if guess ==  name:
+    print ("good job")
+    score += 1
+else:
+    print ("wrong")
+name = "meera"
+guess = input ("this name starts with a meer ")
+if guess == name:
+    print ("good job")
+    score += 1
+else:
+    print ("wrong")
+print (f"game over your score is {score}")
+
+
+
+
+
+
+
